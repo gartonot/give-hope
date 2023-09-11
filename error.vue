3f71e5e7e1f2506029cac9ed7985342edc3c2b0b
vladimir.tkachev <vladimir.tkachev@onlinesup.com>
2023-09-11 21:56:46 +0300

    initial

---
+++ error.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="error-page">404 not found</div>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  ssr: true,
+  typescript: {
+    strict: true
+  },
+  devtools: { enabled: true },
+  components: false,
+  modules: ['nuxt-viewport', 'nuxt-swiper'],
+  app: {
+    head: {
+      htmlAttrs: { lang: 'ru' },
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    }
+  },
+  css: ['~/assets/scss/global.scss', '~/assets/scss/fonts.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "~/assets/scss/variables.scss";'
+        }
+      }
+    }
+  }
+})
