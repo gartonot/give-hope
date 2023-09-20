f0915609c684a780f3529e868939039078a9680e
SuvorovDM <dima9568@gmail.com>
2023-09-21 01:09:07 +0300

    feat: add footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,12 @@
 <script lang="ts" setup>
   import TheHeader from '~/components/TheHeader/Index.vue'
+  import TheFooter from '~/components/TheFooter.vue'
 </script>
 
 <template>
   <div>
     <the-header />
     <slot />
+    <the-footer />
   </div>
 </template>
