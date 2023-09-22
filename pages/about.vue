337e7c936232add1dc9c4bb64b6991fe77520bdd
SuvorovDM <dima9568@gmail.com>
2023-09-23 02:08:44 +0300

    feat: about benefits section

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
   import AboutHeroSection from '~/components/about/AboutHeroSection.vue'
+  import AboutBenefitsSection from '~/components/about/AboutBenefitsSection.vue'
 </script>
 
 <template>
   <main class="about">
     <about-hero-section />
+    <about-benefits-section class="about-benefits" />
   </main>
 </template>
+
+<style scoped lang="scss">
+  .about {
+    padding-block: 40px;
+
+    &-benefits {
+      margin-top: 100px;
+    }
+  }
+</style>
