a8b089e64f8a5b57d999a30fd788b773128f88db
SuvorovDM <dima9568@gmail.com>
2023-09-23 22:28:40 +0300

    feat: about team section

--- pages/about.vue
+++ pages/about.vue
@@ -1,12 +1,16 @@
 <script setup lang="ts">
   import AboutHeroSection from '~/components/about/AboutHeroSection.vue'
   import AboutBenefitsSection from '~/components/about/AboutBenefitsSection.vue'
+  import AboutAmbassadorsSection from '~/components/about/AboutAmbassadorsSection.vue'
+  import AboutTeamSection from '~/components/about/AboutTeamSection.vue'
 </script>
 
 <template>
   <main class="about">
     <about-hero-section />
     <about-benefits-section class="about-benefits" />
+    <about-team-section class="about-benefits" />
+    <about-ambassadors-section class="about-ambassadors" />
   </main>
 </template>
 
@@ -15,6 +19,10 @@
     padding-block: 40px;
 
     &-benefits {
+      margin-top: 100px;
+    }
+
+    &-ambassadors {
       margin-top: 100px;
     }
   }
