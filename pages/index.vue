5be52cf3b8f4708995726db48a8f48b310650522
vladimir.tkachev <vladimir.tkachev@onlinesup.com>
2023-09-24 01:02:19 +0300

    feat: added urgent help section

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,23 @@
 <script lang="ts" setup>
   import MainHero from '~/components/main/MainHero.vue'
+  import MainUrgentHelp from '~/components/main/MainUrgentHelp/Index.vue'
 </script>
 
 <template>
   <main>
     <main-hero />
+    <main-urgent-help v-if="false" class="main__urgent-help" />
   </main>
 </template>
 
 <style scoped lang="scss">
   :global(body) {
     overflow-x: hidden;
+  }
+
+  .main {
+    &__urgent-help {
+      margin-top: 170px;
+    }
   }
 </style>
