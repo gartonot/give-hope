7d6a0647b2dee1d3dd23fda4c86459b166d45752
SuvorovDM <dima9568@gmail.com>
2023-09-24 13:52:41 +0300

    feat: about ambassadors section

--- components/about/AboutAmbassadorsSection.vue
+++ components/about/AboutAmbassadorsSection.vue
@@ -1,7 +1,162 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  const ambassadors = [
+    {
+      name: 'Лаура Джугелия',
+      image: '/images/about/ambassadors/laura.png'
+    },
+    {
+      name: 'Александра Маркина',
+      image: '/images/about/ambassadors/alexandra.png'
+    },
+    {
+      name: 'Наталья Турецкая',
+      image: '/images/about/ambassadors/natalia.png'
+    },
+    {
+      name: 'Кристина Левиева',
+      image: '/images/about/ambassadors/kristina.png'
+    },
+    {
+      name: 'Ксения Шипилова',
+      image: '/images/about/ambassadors/ksenia.png'
+    }
+  ]
+</script>
 
 <template>
-  <section>AboutAmbassadorsSection</section>
+  <section class="ambassadors-section">
+    <div class="container ambassadors-section-container">
+      <h2 class="ambassadors-section__title">Амбассадоры фонда</h2>
+      <div class="ambassadors-section__cards">
+        <div v-for="ambassador in ambassadors" :key="ambassador.name" class="ambassador-card">
+          <img
+            class="ambassador-card__image"
+            :src="ambassador.image"
+            :alt="ambassador.name"
+            loading="lazy"
+          />
+          <p class="ambassador-card__name">
+            {{ ambassador.name }}
+          </p>
+        </div>
+        <img
+          src="/images/about/ambassadors/message-image.svg"
+          alt="message image"
+          loading="lazy"
+          class="ambassador-card message-image"
+        />
+      </div>
+    </div>
+  </section>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+  .ambassadors-section {
+    overflow: hidden;
+
+    .ambassadors-section-container {
+      position: relative;
+
+      &:before {
+        content: '';
+        position: absolute;
+        width: 795px;
+        height: 1027px;
+        left: -300px;
+        background-image: url('/images/about/ambassadors/background-line.png');
+      }
+    }
+
+    &__title {
+      color: $grey-color;
+      font-size: 34px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 111%;
+
+      @media screen and (min-width: $breakpoint-lg) {
+        font-size: 50px;
+      }
+    }
+
+    &__cards {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+      gap: 50px 0;
+      margin-top: 50px;
+      padding-top: 10px;
+      position: relative;
+
+      @media screen and (min-width: $breakpoint-sm) {
+        margin-top: 100px;
+      }
+
+      @media screen and (min-width: $breakpoint-md) {
+        gap: 90px 0;
+      }
+
+      @media screen and (min-width: $breakpoint-md) {
+        gap: 90px 0;
+      }
+    }
+
+    .message-image {
+      object-fit: contain;
+      display: none;
+
+      @media screen and (min-width: $breakpoint-sm) {
+        display: block;
+      }
+    }
+
+    .ambassador-card {
+      width: 100%;
+      text-align: center;
+      position: relative;
+
+      @media screen and (min-width: $breakpoint-sm) {
+        width: 40%;
+      }
+
+      &__image {
+        object-fit: contain;
+        width: 433px;
+        max-height: 280px;
+        min-height: 280px;
+        margin-inline: auto;
+      }
+
+      &__name {
+        color: $grey-color;
+        position: relative;
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: 111%;
+        margin-top: 20px;
+        display: inline-block;
+
+        @media screen and (min-width: $breakpoint-sm) {
+          display: block;
+        }
+
+        @media screen and (min-width: $breakpoint-lg) {
+          font-size: 25px;
+        }
+
+        &:before {
+          content: '';
+          position: absolute;
+          height: 18px;
+          width: 104%;
+          z-index: -1;
+          left: -2%;
+          bottom: 0;
+          background-color: rgba($primary-color, 0.37);
+          transform: rotate(-1deg);
+        }
+      }
+    }
+  }
+</style>
