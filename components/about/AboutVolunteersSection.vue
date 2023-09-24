3526400131a998ac2cf89b641a971bc3a17313aa
SuvorovDM <dima9568@gmail.com>
2023-09-24 14:59:44 +0300

    feat: about volunteers section

---
+++ components/about/AboutVolunteersSection.vue
@@ -0,0 +1,120 @@
+<template>
+  <section class="volunteers-section">
+    <div class="container">
+      <h2 class="volunteers-section__title">Стань частью команды!</h2>
+      <div class="volunteers-section__card">
+        <img src="/images/about/volunteers/avatars.png" alt="avatars" class="card__avatars" />
+        <p class="card__text">
+          Наша команда волонтеров - это замечательная группа людей, объединенных общей целью -
+          помощь тем, кто в ней нуждается. Мы пришли вместе из разных сфер жизни, возрастов и
+          профессий, но нас объединяет желание сделать этот мир лучше.
+        </p>
+        <button class="card__button">Стать волонтером</button>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+  .volunteers-section {
+    padding-bottom: 250px;
+
+    @media screen and (min-width: $breakpoint-md) {
+      padding-bottom: 400px;
+    }
+
+    &__title {
+      color: $grey-color;
+      font-size: 34px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 111%;
+      position: relative;
+      max-width: 400px;
+
+      @media screen and (min-width: $breakpoint-lg) {
+        font-size: 50px;
+      }
+
+      &:before {
+        content: '';
+        position: absolute;
+        height: 18px;
+        width: 104%;
+        z-index: -1;
+        left: -2%;
+        bottom: 0;
+        background-color: rgba($primary-color, 0.37);
+        transform: rotate(-1deg);
+      }
+    }
+
+    &__card {
+      border: 3px solid $blue-color;
+      border-radius: 43px;
+      margin-top: 130px;
+      padding: 110px 20px 80px;
+      position: relative;
+
+      &:before {
+        content: '';
+        position: absolute;
+        background-image: url('/images/about/volunteers/stars.svg');
+        top: -130px;
+        right: 0;
+        width: 201px;
+        height: 201px;
+      }
+      &:after {
+        content: '';
+        position: absolute;
+        background-image: url('/images/about/volunteers/background-image-sm.png');
+        width: 269px;
+        height: 223px;
+        right: 0;
+        left: 0;
+        margin-inline: auto;
+        bottom: -240px;
+
+        @media screen and (min-width: $breakpoint-md) {
+          background-image: url('/images/about/volunteers/background-image-md.png');
+          width: 564px;
+          height: 644px;
+          right: 0;
+          bottom: -360px;
+          left: unset;
+        }
+      }
+    }
+    .card {
+      &__avatars {
+        margin-inline: auto;
+        object-fit: contain;
+      }
+
+      &__text {
+        text-align: center;
+        max-width: 420px;
+        margin-inline: auto;
+        margin-top: 34px;
+        color: $grey-color;
+        font-size: 14px;
+        font-style: normal;
+        font-weight: 500;
+        line-height: 111%;
+      }
+
+      &__button {
+        padding: 18px 62px;
+        border-radius: 25px;
+        border: 1px solid transparent;
+        background-color: $blue-color;
+        color: $white-color;
+        margin-inline: auto;
+        margin-top: 30px;
+        display: flex;
+        white-space: nowrap;
+      }
+    }
+  }
+</style>
--- pages/about.vue
+++ pages/about.vue
@@ -3,6 +3,7 @@
   import AboutBenefitsSection from '~/components/about/AboutBenefitsSection.vue'
   import AboutAmbassadorsSection from '~/components/about/AboutAmbassadorsSection.vue'
   import AboutTeamSection from '~/components/about/AboutTeamSection.vue'
+  import AboutVolunteersSection from '~/components/about/AboutVolunteersSection.vue'
 </script>
 
 <template>
@@ -11,6 +12,7 @@
     <about-benefits-section class="about-benefits" />
     <about-team-section class="about-benefits" />
     <about-ambassadors-section class="about-ambassadors" />
+    <about-volunteers-section class="about-volunteers" />
   </main>
 </template>
 
@@ -24,6 +26,10 @@
 
     &-ambassadors {
       margin-top: 100px;
+    }
+
+    &-volunteers {
+      margin-top: 280px;
     }
   }
 </style>
