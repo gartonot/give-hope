a7fcf65c965249bc71ecd8e73eaa3fd7844cc9a5
SuvorovDM <dima9568@gmail.com>
2023-09-25 02:34:13 +0300

    feat: merch shop section

--- pages/merch.vue
+++ pages/merch.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
   import MerchHeroSection from '~/components/merch/MerchHeroSection.vue'
+  import MerchShopSection from '~/components/merch/MerchShopSection.vue'
 </script>
 
 <template>
   <main class="merch">
     <merch-hero-section />
+    <merch-shop-section />
   </main>
 </template>
 
