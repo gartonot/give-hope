a3aba5416ae2e5e12683530b6668e97bd2ef1ad6
SuvorovDM <dima9568@gmail.com>
2023-09-26 19:52:19 +0300

    feat: help chat

--- app.vue
+++ app.vue
@@ -1,4 +1,9 @@
+<script setup lang="ts">
+  import HelpChat from '~/components/HelpChat.vue'
+</script>
+
 <template>
+  <help-chat />
   <nuxt-layout>
     <nuxt-page />
   </nuxt-layout>
---
+++ components/HelpChat.vue
@@ -0,0 +1,189 @@
+<script setup lang="ts">
+  import CustomCheckbox from '~/components/CustomCheckbox.vue'
+
+  const programs = [
+    {
+      name: 'Пожилым людям',
+      id: 1,
+      image: '/images/fond1.svg',
+      price: 300
+    },
+    {
+      name: 'Уютная старость',
+      id: 2,
+      image: '/images/fond2.svg',
+      price: 300
+    },
+    {
+      name: 'Добро до двери',
+      id: 3,
+      image: '/images/fond3.svg',
+      price: 300
+    },
+    {
+      name: 'Добро до двери',
+      id: 4,
+      image: '/images/fond4.svg',
+      price: 300
+    }
+  ]
+</script>
+
+<template>
+  <div class="help-chat">
+    <div class="help-chat__container">
+      <div class="help-chat__dialog">
+        <custom-checkbox />
+        <p class="help-chat__dialog-title">Выбери программу</p>
+        <button v-for="program in programs" :key="program.id" class="help-chat__dialog-program">
+          <img class="help-chat__dialog-image" :src="program.image" :alt="program.name" />
+          {{ program.name }}
+          <strong>{{ program.price }} ₽</strong>
+        </button>
+
+        <div class="help-chat__dialog-input-wrapper">
+          Другая сумма —
+          <input class="help-chat__dialog-input" type="text" placeholder="500 ₽" />
+        </div>
+        <input
+          class="help-chat__dialog-input-email"
+          type="text"
+          placeholder="Ваш e-mail для чека"
+        />
+        <button class="help-chat__dialog-send">Пожертвовать</button>
+        <p class="help-chat__dialog-description">
+          Совершая пожертвование, вы соглашаетесь с условиями договора-оферты и политики
+          конфиденцальности
+        </p>
+      </div>
+      <div class="help-chat__button-wrapper">
+        <button class="help-chat__button">Помочь</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .help-chat {
+    position: relative;
+
+    &__container {
+      position: fixed;
+      right: 20px;
+      bottom: 10px;
+      z-index: 2;
+
+      @media screen and (min-width: $breakpoint-sm) {
+        right: 50px;
+        bottom: 40px;
+      }
+    }
+
+    &__dialog {
+      padding: 30px 30px 50px;
+      background-color: $blue-dark-color;
+      border-radius: 20px;
+      max-width: 320px;
+      min-width: 320px;
+
+      &-program {
+        width: 100%;
+        margin-top: 12px;
+        background-color: $white-color;
+        border: 1px solid transparent;
+        border-radius: 15px;
+        padding: 10px 20px 10px 10px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+      }
+
+      &-title {
+        color: $white-color;
+        margin-top: 20px;
+      }
+
+      &-image {
+        width: 30px;
+        height: 30px;
+      }
+
+      &-input-wrapper {
+        margin-top: 20px;
+        color: $white-color;
+      }
+
+      &-input {
+        border: 1px solid transparent;
+        border-radius: 15px;
+        background-color: $white-color;
+        padding: 12px;
+        font-size: 20px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: 111%;
+        width: 50%;
+      }
+
+      &-input-email {
+        border: 1px solid transparent;
+        border-radius: 9px;
+        background-color: $white-color;
+        padding: 8px;
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 111%;
+        width: 100%;
+        margin-top: 20px;
+      }
+      &-send {
+        border-radius: 20px;
+        border: 1px solid $white-color;
+        background: linear-gradient(180deg, $blue-color 0%, rgba(120, 168, 239, 0) 100%);
+        color: $white-color;
+        font-size: 25px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: 111%;
+        padding-block: 20px;
+        width: 100%;
+        margin-top: 16px;
+
+        @media screen and (min-width: $breakpoint-sm) {
+          font-size: 20px;
+        }
+      }
+      &-description {
+        color: $white-color;
+        font-size: 12px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 111%;
+        margin-top: 16px;
+      }
+    }
+
+    &__button-wrapper {
+      margin-top: 30px;
+      text-align: end;
+    }
+
+    &__button {
+      border-radius: 20px;
+      border: 1px solid $white-color;
+      background-color: $blue-dark-color;
+      color: $white-color;
+      font-size: 16px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 111%;
+      padding: 10px 30px;
+
+      @media screen and (min-width: $breakpoint-sm) {
+        font-size: 25px;
+        padding: 14px 52px;
+      }
+    }
+  }
+</style>
