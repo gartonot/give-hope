e8e101e8482328b8d0f747a8d934de2768306cbf
SuvorovDM <dima9568@gmail.com>
2023-09-26 03:08:47 +0300

    feat: case gallery section

--- pages/case/[slug].vue
+++ pages/case/[slug].vue
@@ -3,6 +3,7 @@
   import SubscribeNewsletter from '~/components/SubscribeNewsletter.vue'
   import { useRoute } from '#imports'
   import CaseBodySection from '~/components/case/CaseBodySection.vue'
+  import CaseGalerySection from '~/components/case/CaseGalerySection.vue'
 
   const route = useRoute()
   const slug = route.params.slug as string
@@ -22,6 +23,17 @@
           'Программа "Купить еды" работает по принципу сбора пожертвований от добровольцев, которые затем используются для покупки еды для нуждающихся людей. Средства могут быть использованы для покупки продуктов питания или семян для выращивания собственной пищи в рамках устойчивого сельского хозяйства.</br></br>' +
           'Эта благотворительная программа работает в тесном партнерстве с местными организациями, такими как приюты для бездомных, церкви, некоммерческие организации и общественные фонды. Они предоставляют информацию о семьях и отдельных людях, которым необходима помощь, и координируют доставку продуктов до их дверей. Благодаря программе "Купить еды" у голодающих людей есть возможность получить достойное питание, которое позволяет им сохранять здоровье и продуктивность. Это также позволяет семьям сэкономить средства на покупке еды, которые могут быть использованы для других потребностей, таких как образование, медицинская помощь или жилище.</br></br>' +
           'Важной особенностью этой программы является прозрачность использования пожертвований. Добровольцы могут быть уверены, что их пожертвования будут использованы эффективно и надлежащим образом, чтобы помочь тем, кто в действительности нуждается в поддержке.'
+      },
+      gallerySection: {
+        title: 'Галерея',
+        images: [
+          '/images/case/case-image1.png',
+          '/images/case/case-image2.png',
+          '/images/case/case-image3.png',
+          '/images/case/case-image1.png',
+          '/images/case/case-image2.png',
+          '/images/case/case-image3.png'
+        ]
       }
     }
   }
@@ -33,7 +45,8 @@
   <main class="case">
     <case-hero-section :settings="currentCase.heroSection" />
     <case-body-section :settings="currentCase.bodySection" class="body-section" />
-    <subscribe-newsletter />
+    <case-galery-section :settings="currentCase.gallerySection" class="gallery-section" />
+    <subscribe-newsletter class="subscribe-section" />
   </main>
 </template>
 
@@ -50,6 +63,18 @@
 
       @media screen and (min-width: $breakpoint-lg) {
         margin-top: 160px;
+      }
+    }
+
+    .gallery-section {
+      margin-top: 130px;
+    }
+
+    .subscribe-section {
+      margin-top: 100px;
+
+      @media screen and (min-width: $breakpoint-lg) {
+        margin-top: 200px;
       }
     }
   }
